00cf9a3cf1e528ae188026fa8d1c04fba13345ae
emma bridgers <emma@miwa.moe>
2021-07-16 16:25:28 -0500

    using 'into' now when generating web api responses

--- src/api/models/replies.rs
+++ src/api/models/replies.rs
@@ -1,4 +1,7 @@
 use serde::{Serialize, Deserialize};
+use harsh::Harsh;
+use rocket::State;
+use super::{Episode, Season};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Series {
@@ -8,7 +11,7 @@ pub struct Series {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Season {
+pub struct ReturnSeason {
     pub id: String,
     pub series_id: String,
     pub slug: String,
@@ -23,11 +26,53 @@ pub struct Season {
     pub episode_dur: Option<i32>
 }
 
+impl From<Season> for ReturnSeason{
+    fn from(season:Season) -> ReturnSeason{
+        let hashid = create_hashid_client();
+        ReturnSeason {
+            id: hashid.encode(&[season.id as u64]),
+            series_id: hashid.encode(&[season.series_id as u64]),
+            slug: season.slug,
+            title_en: season.title_en,
+            title_ja: season.title_ja,
+            title_romaji: season.title_romaji,
+            keywords: season.keywords,
+            anilist_id: season.anilist_id,
+            description: season.description,
+            synonyms: season.synonyms,
+            episode_amt: season.episode_amt,
+            episode_dur: season.episode_dur
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct Episode {
+pub struct ReturnEpisode {
     pub id: String,
     pub season_id: String,
     pub number: f64,
     pub title: Option<String>,
     pub description: Option<String>,
+}
+
+impl From<&Episode> for ReturnEpisode{
+    fn from(episode:&Episode) -> ReturnEpisode{
+        let hashid = create_hashid_client();
+        ReturnEpisode {
+            id: hashid.encode(&[episode.id as u64]),
+            season_id: hashid.encode(&[episode.season_id as u64]),
+            number: episode.number,
+            title: episode.title.clone(),
+            description: episode.description.clone(),
+        }
+    }
+}
+
+fn create_hashid_client() -> Harsh {
+    let hashid = Harsh::builder()
+    .length(5)
+    .salt("sakura-app!")
+    .build()
+    .unwrap();
+    return hashid
 }
