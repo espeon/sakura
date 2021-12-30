55bd86d6500e75792a07844251d95dbe01ae4d5e
natalie <22222885+kanbaru@users.noreply.github.com>
2021-12-29 18:49:44 -0600

    Removed showing password in logs

--- src/db.rs
+++ src/db.rs
@@ -8,9 +8,12 @@ pub async fn get_pool() -> anyhow::Result<PgPool, anyhow::Error> {
         .max_lifetime(std::time::Duration::from_secs(10))
         .connect(&env::var("DATABASE_URL")?)
         .await?;
-    println!(
-        "Connected to the database at url {}",
-        &env::var("DATABASE_URL")?
-    );
+    let url = &env::var("DATABASE_URL")?;
+    let mut split = url.split("@");
+    let mut creds = split.next().unwrap().split("://");
+    creds.next();
+    let username = creds.next().unwrap().split(":").next().unwrap();
+    let cleaned_url = split.next().unwrap().split("/").next().unwrap();
+    println!("Connected to the database at {} as {}", &cleaned_url, username);
     Ok(pool)
 }
